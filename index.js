171a99d9deaba6f1106027ebc4cbcd6266eb7a65
ShabirK21 <shaber213@gmail.com>
2022-03-27 18:46:32 +0800

    updated asset name and changed paths

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const indexD = {
     {
       title: "Norwegian Wood",
       author: "Haruki Murakami",
-      img: "./Assets/HM.jpg",
+      img: "./Assets/hm-1.jpg",
       content:
         "Join Haruki Murakami as he discusses his award winning novel, Norwegian Wood.",
       link: "./Articles/a1/a1.html",
@@ -13,7 +13,7 @@ const indexD = {
     {
       title: "The Hobbit",
       author: "J.R.R. Tolkien",
-      img: "./Assets/JRR.jpg",
+      img: "./Assets/jrr-1.jpg",
       content:
         "Find out how renown author J.R.R Tolkien got inspired to write, The Hobbit",
       link: "./Articles/a2/a2.html",
@@ -21,7 +21,7 @@ const indexD = {
     {
       title: "When Breath Becomes Air",
       author: "Paul Kalanithi",
-      img: "./Assets/PK.jpg",
+      img: "./Assets/pk-1.jpg",
       content:
         "Keeping Dr.Paul Kalanithi's Voice Alive, Lucy Kalanithi talks about her husband's life ",
       link: "./Articles/a3/a3.html",
@@ -29,7 +29,7 @@ const indexD = {
     {
       title: "The Alchemist",
       author: "Paulo Coelho",
-      img: "./Assets/PC.jpg",
+      img: "./Assets/pc-1.jpg",
       content:
         "Over 35 million copies sold, Paulo Coelho discusses his sleeper hit, The Alchemist",
       link: "./Articles/a4/a4.html",
@@ -37,7 +37,7 @@ const indexD = {
     {
       title: "The Kite Runner",
       author: "Khaled Hosseini",
-      img: "./Assets/KH.jpg",
+      img: "./Assets/kh-1.jpg",
       content:
         "Written in the early mornings before work as a doctor, Khaled Hosseini talks about his journey writing The Kite Runner",
       link: "./Articles/a5/a5.html",
